d00f6397aac6eb6b1c0377db237a74ab396f61cd
Mosh <126637500+Kaymook@users.noreply.github.com>
2024-05-20 21:08:38 +0900

    Add files via upload

---
+++ minishell.h
@@ -0,0 +1,19 @@
+#ifndef MINISHELL_H
+# define MINISHELL_H
+
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <fcntl.h>
+# include <sys/types.h>
+# include <sys/wait.h>
+# include <sys/stat.h>
+# include <string.h>
+# include <errno.h>
+# include <stdbool.h>
+# include <signal.h>
+# include <readline/readline.h>
+# include <readline/history.h>
+# include "libft/libft.h"
+
+#endif
